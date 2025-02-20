aec616edf11ea45b32cf95c7c54c3151b8271b77
Peter <szpeto12@gmail.com>
2025-02-20 21:35:21 +0100

    Final working version v2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,45 @@
 
 public class Main {
 
+	public static void main(String [] args) {
+		
+		
+		
+	}
+	
+    public static int maxArea(int[] height) {
+        
+    	int i = 0;
+    	int j = height.length -1;
+    	int result = 0;
+    	int tempResult;
+    	
+    	while(j > i) {
+    		
+    		if(height[i] > height[j]) {
+    			
+    			tempResult = (j - i) * height[j];
+    			if(tempResult > result) result = tempResult;
+    			j--;
+    			
+    		} else if(height[i] == height[j]) {
+    			
+    			tempResult = (j - i) * height[i];
+    			if(tempResult > result) result = tempResult;
+    			i++;
+    			
+    		} else {
+    			
+    			tempResult = (j - i) * height[i];
+    			if(tempResult > result) result = tempResult;
+    			i++;
+    			
+    		}
+    		
+    	}
+    	
+    	return result;
+    	
+    }
+	
 }
